ea5b4f2bf882f2ad07b4878ce36b05afff18c785
Paul Krohn <paul@daemonize.com>
2015-03-14 21:34:13 -0700

    add 2/3 screens and full screen.

--- slate.js
+++ slate.js
@@ -1,3 +1,10 @@
+var fullScreen = slate.operation("move", {
+  "x" : "screenOriginX",
+  "y" : "screenOriginY",
+  "width" : "screenSizeX",
+  "height" : "screenSizeY"
+});
+
 var pushLeftThird = slate.operation("push", {
   "direction" : "left",
   "style" : "bar-resize:screenSizeX/3"
@@ -6,6 +13,16 @@ var pushLeftThird = slate.operation("push", {
 var pushRightThird = slate.operation("push", {
   "direction" : "right",
   "style" : "bar-resize:screenSizeX/3"
+});
+
+var pushRight2Thirds = slate.operation("push", {
+  "direction" : "right",
+  "style" : "bar-resize:2*screenSizeX/3"
+});
+
+var pushLeft2Thirds = slate.operation("push", {
+  "direction" : "left",
+  "style" : "bar-resize:2*screenSizeX/3"
 });
 
 var pushCenterThird = slate.operation("move", {
@@ -39,4 +56,16 @@ slate.bind("left:shift;ctrl", function(win) {
 
 slate.bind("right:shift;ctrl", function(win) {
     win.doOperation(pushRightThird);
+});
+
+slate.bind("right:shift;ctrl;alt",function(win) {
+    win.doOperation(pushRight2Thirds);
+});
+
+slate.bind("left:shift;ctrl;alt",function(win) {
+    win.doOperation(pushLeft2Thirds);
+});
+
+slate.bind("f:alt;ctrl",function(win) {
+    win.doOperation(fullScreen);
 });
