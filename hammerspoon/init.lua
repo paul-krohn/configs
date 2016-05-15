d34bdacdb6610a7df77ef41f6f3435369315facc
Paul Krohn <pkrohn@daemonize.com>
2016-05-15 15:11:13 -0700

    add window movers, ps1

---
+++ hammerspoon/init.lua
@@ -0,0 +1,112 @@
+--[[
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "H", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+
+  f.x = f.x - 10
+  win:setFrame(f)
+end)
+--]]
+-- reload config
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "R", function()
+  hs.reload()
+end)
+hs.alert.show("Config loaded")
+
+-- left third
+hs.hotkey.bind({"alt", "ctrl"}, "U", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x
+  f.y = max.y
+  f.w = max.w / 3
+  f.h = max.h
+  win:setFrame(f)
+end)
+
+-- left two thirds
+hs.hotkey.bind({"alt", "ctrl"}, "I", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x
+  f.y = max.y
+  f.w = 2 * max.w / 3
+  f.h = max.h
+  win:setFrame(f)
+end)
+
+-- right two thirds
+hs.hotkey.bind({"alt", "ctrl"}, "O", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x + (max.w / 3)
+  f.y = max.y
+  f.w = 2 * max.w / 3
+  f.h = max.h
+  win:setFrame(f)
+end)
+
+-- right third
+hs.hotkey.bind({"alt", "ctrl"}, "P", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x + 2 * (max.w / 3)
+  f.y = max.y
+  f.w = max.w / 3
+  f.h = max.h
+  win:setFrame(f)
+end)
+
+-- upper right third
+hs.hotkey.bind({"alt", "ctrl"}, ";", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x + 2 * (max.w / 3)
+  f.y = max.y / 2
+  f.w = max.w / 3
+  f.h = max.h / 2
+  win:setFrame(f)
+end)
+
+-- lower right third
+hs.hotkey.bind({"alt", "ctrl"}, "l", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x + 2 * (max.w / 3)
+  f.y = max.y
+  f.w = max.w / 3
+  f.h = max.h / 2
+  win:setFrame(f)
+end)
+
+-- full screen
+hs.hotkey.bind({"alt", "ctrl"}, "f", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x
+  f.y = max.y
+  f.w = max.w
+  f.h = max.h
+  win:setFrame(f)
+end)
