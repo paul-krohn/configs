83538cb61010e80fa9bfae4ed44883c2c23f4872
Paul Krohn <pkrohn@daemonize.com>
2020-03-15 20:15:41 -0700

    convert to MiroWindowsManager

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,118 +1,128 @@
--- reload config
+--- reload config
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "R", function()
   hs.reload()
 end)
 hs.alert.show("Config loaded")
 
-function screenFrameHeightDiff(win)
-  local screen = win:screen()
-  local fr = screen:frame()
-  local ff = screen:fullFrame()
-  return ff.y - fr.y
+local v60hyper = {}
+local globalHyper = {"ctrl", "alt"}
+local primaryScreen = hs.screen.primaryScreen()
+local primaryScreenMenuBarOffset = primaryScreen:frame().y
+
+hs.loadSpoon("MiroWindowsManager")
+-- the notation here is confusing -- the numerator and denominator are revered.
+-- also be sure the grid size is divisible by the grid size _for height and width_.
+spoon.MiroWindowsManager.sizes = {12/7, 2, 12/5}
+
+hs.grid.MARGINX = 12
+hs.grid.MARGINY = 12
+-- setting animationDuration to not-zero makes iTerm/Terminal windows move all herky-jerky
+hs.window.animationDuration = 0.0
+
+-- bindings for my custom-programmed keyboard
+spoon.MiroWindowsManager:bindHotkeys({
+  -- these keys are all mapped under layer 2
+  -- f14 is w/up-arrow
+  up = {v60hyper, "f14"},
+  --pad+ is s/down-arrow
+  down = {v60hyper, "pad+"},
+  -- pad- is a/left-arrow
+  left = {v60hyper, "pad-"},
+  -- padenter is d/right-arrow
+  right = {v60hyper, "padenter"},
+  -- pad1 is f
+  fullscreen = {v60hyper,"pad1"}
+})
+-- bindings for an Apple keyboard, like the built-in one on a laptop
+spoon.MiroWindowsManager:bindHotkeys({
+  up = {globalHyper, "up"},
+  down = {globalHyper, "down"},
+  left = {globalHyper, "left"},
+  right = {globalHyper, "right"},
+  fullscreen = {globalHyper, "f"}
+})
+
+function stackWindows(win)
+  -- find all windows in the app of the frontmost window
+  -- make all the windows in the app the same size
+  local f = win:frame()
+  local app = win:application()
+  local windows = app:allWindows()
+  for i, window in ipairs(windows) do
+    window:setFrame(f)
+  end
 end
 
--- define sizes of the 'large' vs 'small' left/right sections
-left_third = 0.45
-middle_third = 0.38
-right_third = 0.26
-padding_size = 20
-
--- full screen
-hs.hotkey.bind({"alt", "ctrl"}, "s", function()
+hs.hotkey.bind(v60hyper, "f17", function()
   local win = hs.window.focusedWindow()
-  local f = win:frame()
-  local screen = win:screen()
-  local max = screen:frame()
-
-  f.x = max.x
-  f.y = max.y
-  f.w = max.w
-  f.h = max.h
-  win:setFrame(f)
+  stackWindows(win)
 end)
 
--- half left
-hs.hotkey.bind({"alt", "ctrl"}, "j", function()
-  local win = hs.window.focusedWindow()
-  local f = win:frame()
-  local screen = win:screen()
-  local max = screen:frame()
+maxFrame = {
+  x = 0,
+  y = primaryScreenMenuBarOffset,
+  h = primaryScreen:frame().h,
+  w = primaryScreen:frame().w,
+}
 
-  f.x = max.x
-  f.y = max.y
-  f.w = max.w / 2
-  f.h = max.h
-  win:setFrame(f)
+lowerRight = {
+  x = maxFrame.w / 2,
+  y = 2 * maxFrame.h / 3 + primaryScreenMenuBarOffset,
+  w = maxFrame.w / 2,
+  h = maxFrame.h / 3,
+}
+
+upperRight = {
+  x = maxFrame.w / 2,
+  y = primaryScreenMenuBarOffset,
+  w = maxFrame.w / 2,
+  h = 2* maxFrame.h / 3,
+}
+
+halfLeft = {
+  x = 0,
+  y = primaryScreenMenuBarOffset,
+  w = maxFrame.w / 2,
+  h = maxFrame.h,
+}
+
+local appPositions = {
+  Atom = halfLeft,
+  ['Google Chrome'] = halfLeft,
+  ['IntelliJ IDEA'] = halfLeft,
+  iTerm2 = upperRight,
+  -- Messages = lowerRight,
+  PyCharm = halfLeft,
+  Slack = lowerRight,
+}
+
+hs.hotkey.bind(globalHyper, "k", function()
+  -- print("setting up default window positions")
+  -- print(appPositions.PyCharm.x)
+  for appName, position in pairs(appPositions) do
+    -- print("for ", appName)
+    thisApp = hs.application.get(appName)
+    if thisApp == nil then
+      print("skipping nil app: ", appName)
+    else
+      for title, appWindow in pairs(thisApp:allWindows()) do
+        print("the position for ", title, " is h/w/x/y", position.h, position.w, position.x, position.y)
+        appWindow:setFrame(position)
+      end
+    end
+  end
 end)
 
--- half right
-hs.hotkey.bind({"alt", "ctrl"}, "k", function()
+-- the 'pad=' key is under 'n' on layer 2
+hs.hotkey.bind(v60hyper, "pad=", function()
   local win = hs.window.focusedWindow()
-  local f = win:frame()
-  local screen = win:screen()
-  local max = screen:frame()
-
-  f.x = max.x + max.w / 2
-  f.y = max.y
-  f.w = max.w / 2
-  f.h = max.h
-  win:setFrame(f)
-end)
-
--- left "third"
-hs.hotkey.bind({"alt", "ctrl"}, "a", function()
-  local win = hs.window.focusedWindow()
-  local f = win:frame()
-  local screen = win:screen()
-  local max = screen:frame()
-
-  f.x = max.x + padding_size
-  -- f.y = max.y
-  f.y = 2 * padding_size
-  f.w = max.w * left_third - padding_size
-  f.h = max.h - 2 * padding_size
-  win:setFrame(f)
-end)
-
-
--- middle "third"
-hs.hotkey.bind({"alt", "ctrl"}, "w", function()
-  local win = hs.window.focusedWindow()
-  local f = win:frame()
-  local screen = win:screen()
-  local max = screen:frame()
-
-  f.x = max.x + max.w * left_third + padding_size
-  f.y = 2 * padding_size
-  f.w = max.w * (1 - left_third) - 2 * padding_size
-  f.h = max.h / 2 - padding_size
-  win:setFrame(f)
-end)
-
--- right "third"
-hs.hotkey.bind({"alt", "ctrl"}, "x", function()
-  local win = hs.window.focusedWindow()
-  local f = win:frame()
-  local screen = win:screen()
-  local max = screen:frame()
-
-  f.x = max.x + max.w * left_third + padding_size
-  f.y = max.h / 2 - screenFrameHeightDiff(win) + padding_size
-  f.w = max.w * (1 - left_third) - 2 * padding_size
-  f.h = max.h / 2 - (2 * padding_size)
-  win:setFrame(f)
-end)
-
--- right  two thirds
-hs.hotkey.bind({"alt", "ctrl"}, "d", function()
-  local win = hs.window.focusedWindow()
-  local f = win:frame()
-  local screen = win:screen()
-  local max = screen:frame()
-
-  f.x = max.x + max.w * left_third + padding_size
-  f.y = 2 * padding_size
-  f.w = max.w * (1 - left_third) - 2 * padding_size
-  f.h = max.h - 2 * padding_size
-  win:setFrame(f)
+  local scr = win:screen()
+  local nextScreen = scr:next()
+  win:moveToScreen(scr:next())
+  if nextScreen:fullFrame().h * nextScreen:fullFrame().w < scr:fullFrame().h * scr:fullFrame().w then
+    print(string.format("next screen area: %s", nextScreen:fullFrame().h * nextScreen:fullFrame().w))
+    print(string.format("this screen area: %s", scr:fullFrame().h * scr:fullFrame().w))
+    -- next screen is smaller; make it MiroWindowsManager's idea of full screen.
+    spoon.MiroWindowsManager:_nextFullScreenStep()
+  end
 end)
