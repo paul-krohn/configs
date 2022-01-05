856168fbb82c7e0ba0ea7f1b494fad096f3d584e
Paul Krohn <pkrohn@salesforce.com>
2022-01-05 13:28:19 -0800

    these dimensions were great for my older/smaller screen

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -39,7 +39,6 @@ macro_mash = {"ctrl", "alt"}
 
 spoon.PaulWindowManager:bindKeys{
   sizes = {
-<<<<<<< HEAD
     {mash = macro_mash, key = "q", size = {h = 65, w = 55}},
     {mash = macro_mash, key = "w", size = {h = 65}},
     {mash = macro_mash, key = "e", size = upperRight},
@@ -51,28 +50,6 @@ spoon.PaulWindowManager:bindKeys{
     {mash = macro_mash, key = "z", size = {h = 35, w = 55,         y = 65}},
     {mash = macro_mash, key = "x", size = {h = 35,                 y = 65}},
     {mash = macro_mash, key = "c", size = lowerRight},
-=======
-    {mash = {},              key = "f13",  size = {h = 65, w = 50}},
-    {mash = {"ctrl", "alt"}, key = "q",    size = {h = 65, w = 50}},
-    {mash = {},              key = "f14",  size = {h = 65}},
-    {mash = {"ctrl", "alt"}, key = "w",    size = {h = 65}},
-    {mash = {},              key = "f15",  size = upperRight},
-    {mash = {"ctrl", "alt"}, key = "e",    size = upperRight},
-
-    {mash = {},              key = "pad-", size = halfLeft},
-    {mash = {"ctrl", "alt"}, key = "a",    size = halfLeft},
-    {mash = {},              key = "pad+", size = {}},
-    {mash = {"ctrl", "alt"}, key = "s",    size = {}},
-    {mash = {},              key = "padenter", size = {    w = 50, x = 50}},
-    {mash = {"ctrl", "alt"}, key = "d",    size = {        w = 50, x = 50}},
-
-    {mash = {},              key = "pad7", size = {h = 35, w = 50,         y = 65}},
-    {mash = {"ctrl", "alt"}, key = "z",    size = {h = 35, w = 50,         y = 65}},
-    {mash = {},              key = "pad8", size = {h = 35,                 y = 65}},
-    {mash = {"ctrl", "alt"}, key = "x",    size = {h = 35,                 y = 65}},
-    {mash = {},              key = "pad9", size = lowerRight},
-    {mash = {"ctrl", "alt"}, key = "c",    size = lowerRight},
->>>>>>> f8a0646 (adjust sizes for larger screen)
   },
   deltas = {
     {mash = macro_mash, key = "up", delta = 10, hw = 'h'},
