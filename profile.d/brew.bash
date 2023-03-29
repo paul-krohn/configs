e1acfe71b703a45f6fe98c8407f73bedb1403720
Paul Krohn <pkrohn@salesforce.com>
2023-03-29 14:59:20 -0700

    add all the things that had piled up

---
+++ profile.d/brew.bash
@@ -0,0 +1,1 @@
+export PATH=/opt/homebrew/opt/gnu-sed/libexec/gnubin:/opt/homebrew/opt/gnu-getopt/bin:/opt/homebrew/bin:/opt/homebrew/opt/node@14/bin:/Users/pkrohn/bin:$PATH
---
+++ profile.d/minikube.bash
@@ -0,0 +1,7 @@
+
+eval $(minikube -p minikube docker-env 2>/dev/null)
+if [ $? -eq 0 ] ; then
+  alias k='kubectl '
+else
+  echo "docker or minikube not running. Run \$(minikube -p minikube docker-env) to enable all the things." 1>&2
+fi
