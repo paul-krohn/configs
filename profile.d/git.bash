d344ec9d3caea69032871782d8efc4eab787aff8
Paul Krohn <pkrohn@salesforce.com>
2023-05-16 14:26:56 -0700

    git autocomplete

---
+++ profile.d/git.bash
@@ -0,0 +1,5 @@
+
+if [ -f /opt/homebrew/etc/bash_completion.d/git-completion.bash ]; then
+    # shellcheck source=/opt/homebrew/etc/bash_completion.d/git-completion.bash
+  . /opt/homebrew/etc/bash_completion.d/git-completion.bash
+fi
