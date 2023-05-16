eeb8dc3e5ea8c2ce5bcd02d46decbeb2d8c11b06
Paul Krohn <pkrohn@salesforce.com>
2023-05-16 14:25:53 -0700

    add minikube and alias

--- profile.d/minikube.bash
+++ profile.d/minikube.bash
@@ -1,7 +1,9 @@
 
-eval $(minikube -p minikube docker-env 2>/dev/null)
+eval "$(minikube -p minikube docker-env 2>/dev/null)"
 if [ $? -eq 0 ] ; then
   alias k='kubectl '
 else
   echo "docker or minikube not running. Run \$(minikube -p minikube docker-env) to enable all the things." 1>&2
 fi
+
+alias k='kubectl '
