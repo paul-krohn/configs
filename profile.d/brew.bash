929917fb7bb45e989b925c12cd88f085d506c3a7
Paul Krohn <pkrohn@salesforce.com>
2023-08-03 09:29:00 -0700

    i forget why I want asdf but there it is

--- profile.d/brew.bash
+++ profile.d/brew.bash
@@ -1,1 +1,5 @@
+# shellcheck shell=bash
 export PATH=/opt/homebrew/opt/gnu-sed/libexec/gnubin:/opt/homebrew/opt/gnu-getopt/bin:/opt/homebrew/bin:/opt/homebrew/opt/node@14/bin:/Users/pkrohn/bin:$PATH
+
+# asdf needs `brew` in the path
+. $(brew --prefix asdf)/libexec/asdf.sh
