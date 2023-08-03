a75d96924e01374a08d49404f09dc5cc939062b9
Paul Krohn <pkrohn@salesforce.com>
2023-08-03 09:34:16 -0700

    add git push alias

--- profile.d/git.bash
+++ profile.d/git.bash
@@ -3,3 +3,5 @@ if [ -f /opt/homebrew/etc/bash_completion.d/git-completion.bash ]; then
     # shellcheck source=/opt/homebrew/etc/bash_completion.d/git-completion.bash
   . /opt/homebrew/etc/bash_completion.d/git-completion.bash
 fi
+
+alias gpoh='git push origin HEAD'
