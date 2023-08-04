531643d868d2f3ea5ed214de82409d08d16fa156
Paul Krohn <pkrohn@salesforce.com>
2023-08-04 15:29:20 -0700

    there is no pleasing shellcheck sometimes

--- profile.d/git.bash
+++ profile.d/git.bash
@@ -1,6 +1,6 @@
 
 if [ -f /opt/homebrew/etc/bash_completion.d/git-completion.bash ]; then
-    # shellcheck source=/opt/homebrew/etc/bash_completion.d/git-completion.bash
+  # shellcheck source=/opt/homebrew/etc/bash_completion.d/git-completion.bash
   . /opt/homebrew/etc/bash_completion.d/git-completion.bash
 fi
 
