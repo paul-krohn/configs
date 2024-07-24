c6a0e77232ab78db774d1a515b1443933286d574
Paul Krohn <pkrohn@salesforce.com>
2024-07-24 13:23:37 -0700

    golang setup to start

---
+++ profile.d/golang.bash
@@ -0,0 +1,4 @@
+export GONOPROXY=git.soma.salesforce.com
+export GONOSUMDB=git.soma.salesforce.com
+export GOPRIVATE=git.soma.salesforce.com
+export GOPROXY=https://nexus-proxy.repo.local.sfdc.net/nexus/repository/go-proxy/
