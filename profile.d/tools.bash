dcf1957b7ea9fcc556bd93defcf31732796e60df
Paul Krohn <pkrohn@salesforce.com>
2025-02-13 09:57:36 -0800

    add PATH that pipx uses

--- profile.d/tools.bash
+++ profile.d/tools.bash
@@ -1,2 +1,5 @@
 # shellcheck shell=bash
 export PATH=$PATH:${HOME}/source/tools/shell/bin:${HOME}/source/fedx-validation-wrapper/bin
+
+# pipx puts things here
+export PATH=$PATH:/Users/pkrohn/.local/bin
